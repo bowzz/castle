45f3f1baf8712d2cc30ecf11c371e0b51c1798be
Carson Hull <carsonhull@nicoles-mbp-2.ad.uvu.edu>
2020-04-27 14:06:26 -0600

    worked with events

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -7,8 +7,6 @@ console.log(document.querySelector('.heyo'))
 
 let heyoDiv = document.querySelector('.heyo')
 
-heyoDiv.textContent = "Star Wars Cast" 
-
 let castList = document.createElement("ul")
 
 let counter = 1
@@ -17,9 +15,22 @@ people.forEach(person => {
     let nameItem = document.createElement("li")
     nameItem.textContent = person.name
     castList.appendChild(nameItem)
+
+    let personAnchor = document.createElement("a")
+    personAnchor.href = '#'
     let personImg = document.createElement("img")
     personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
-    heyoDiv.appendChild(personImg)
+
+    personImg.addEventListener('error', (event) => {
+        personImg.hidden = true
+       //personImg.src = '../images/uvu.jpg'  
+    });
+    
+    personImg.addEventListener("click", function( event ) {
+    console.log('thanks for clicking me')
+    })
+    personAnchor.appendChild(personImg)
+    heyoDiv.appendChild(personAnchor)
     counter++
 })
 
