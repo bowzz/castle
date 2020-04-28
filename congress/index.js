4bf0d842e4c04f9f646d6cded36fc45d10fc5d03
Carson Hull <carsonhull@Nicoles-MacBook-Pro-2.local>
2020-04-28 11:16:23 -0600

    senators started

---
+++ congress/index.js
@@ -0,0 +1,15 @@
+import { senators } from '../data/senators.js'
+
+console.log(senators.length)
+
+// Filter, map reduce 
+
+const container = document.querySelector('.container')
+
+const filterSenators = (prop,value) => {
+    return senators.filter(senator => senator[prop] === value )
+}
+
+console.log(filterSenators('party','R'))
+console.log(filterSenators('party','D'))
+console.log(filterSenators('party','ID'))
