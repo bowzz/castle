f357b3e195faaeea558e91fa7016b06e81d6fcfc
Carson Hull <carsonhull@Nicoles-MacBook-Pro-2.local>
2020-04-28 02:05:57 -0600

    starfield backgroun and info

--- starships/main.js
+++ starships/main.js
@@ -1,11 +1,25 @@
 import { starships } from '../data/starships.js'
-import { removeChildren, getLastNumber } from '../utils.js'
+import { removeChildren, getLastNumber, addStarField } from '../utils.js'
 
 const nav = document.querySelector('.nav')
 
 const navList = document.querySelector('.navList')
 
-const shipView = document.querySelector('#main')
+const shipView = document.querySelector('.shipView')
+
+const modalDialog = document.querySelector('.modal')
+const modalButton = document.querySelector('.modal-close')
+const modalBackground =  document.querySelector('.modal-background')
+const modalMessage = document.querySelector('.dialogMessage')
+
+modalButton.addEventListener('click', () => {
+    modalDialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    modalDialog.classList.toggle("is-active")
+})
+
 
 function populateNav(starships) {
     starships.forEach(starship => {
@@ -15,6 +29,7 @@ function populateNav(starships) {
         listItem.textContent = starship.name
 
 shipAnchor.addEventListener('click', event => {
+
     //stores the name of the list clicked on
     let shipName = event.target.textContent
     const foundShip = starships.find(ship => ship.name === shipName);
@@ -22,6 +37,9 @@ shipAnchor.addEventListener('click', event => {
     populateShipView(foundShip)
     
 })
+
+
+
 
         shipAnchor.appendChild(listItem)
         navList.appendChild(shipAnchor)
@@ -35,9 +53,18 @@ function populateShipView(shipData) {
     let imageNum = getLastNumber(shipData.url)
     let shipImage = document.createElement('img')
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${imageNum}.jpg`
+
+    shipImage.addEventListener('error', (event) => {
+        shipImage.hidden = true
+        modalDialog.classList.toggle("is-active")
+        modalMessage.textContent = `Sorry Couldn't Find your Image of ${shipData.name}`
+    })
+   
     shipView.appendChild(shipImage)
 }
 
 populateNav(starships)
+
+addStarField(document.body, )
 
 
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
-export function removeChildren(element){
-    while (element.firstChild){
+export function removeChildren(element) {
+    while (element.firstChild) {
         element.removeChild(element.
             firstChild)
     }
@@ -12,4 +12,19 @@ export function getLastNumber(url) {
         start++
     }
     return url.slice(start, end)
+}
+
+export function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('position', 'absolute')
+        star.style.setProperty('width', '15px')
+        star.style.setProperty('height', '15px')
+        star.style.setProperty('background-color', 'white')
+        star.style.left = `100px`
+        star.style.top = `100px`
+        element.appendChild(star)
+    }
+
 }
