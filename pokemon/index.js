eb55bde184034e29bff63f53d7ec44ac586a082a
Carson Hull <carsonhull@Nicoles-MacBook-Pro-2.local>
2020-04-28 18:42:42 -0600

    imprioved api for poke data

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,7 +1,7 @@
 
 let pokeContainer = document.querySelector('.pokeContainer')
 
-function getPokeData(url) {
+/* function getPokeData(url) {
     fetch(url).then(function (response) {
         response.json().then(function (pokemon) {
             console.log(pokemon.results)
@@ -10,29 +10,58 @@ function getPokeData(url) {
     })
 }
 
-getPokeData("https://pokeapi.co/api/v2/pokemon")
+getPokeData("https://pokeapi.co/api/v2/pokemon") */
 
 //https://pokeapi.co/api/v2/pokemon/1/
 
-function populatePokeCards(pokeArray) {
-    pokeArray.forEach((pokemon) => {
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = response.json()
+        return data
+
+    }
+
+    catch (error) {
+        console.error(error)
+    }
+}
+
+getAPIData('http://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+    (data) => {
+        for (const pokemon of data.results) {
+            getAPIData(pokemon.url).then(
+                (pokeData) => {
+                    populatePokeCard(pokeData)
+                }
+            )
+        }
+    }
+)
+
+
+function populatePokeCard(singlePokemon) {
         let pokeScene = document.createElement('div')
-    pokeScene.className = 'scene'
-    let pokeCard = document.createElement('div')
-    pokeCard.className = 'card'
-    let pokeFront = document.createElement('div')
-    pokeFront.className = 'card__face card__face--front'
-    pokeFront.textContent = "Front"
-    let pokeBack = document.createElement('div')
-    pokeBack.className = 'card__face card__face--back'
-    pokeBack.textContent = "Back!"
+        pokeScene.className = 'scene'
+        let pokeCard = document.createElement('div')
+        pokeCard.className = 'card'
+        pokeCard.addEventListener('click', function () {
+            pokeCard.classList.toggle('is-flipped');
+        });
 
-    pokeContent.appendChild(pokeFront)
-    pokeContent.appendChild(pokeBack)
-    pokeCard.appendChild(pokeContent)
-    pokeContainer.appendChild(pokeCard) 
+        let pokeFront = document.createElement('div')
+        pokeFront.className = 'card__face card__face--front'
+        pokeFront.textContent = singlePokemon.name
+        let pokeBack = document.createElement('div')
+        pokeBack.className = 'card__face card__face--back'
+        pokeBack.textContent = "Back!"
 
-})
+        pokeCard.appendChild(pokeFront)
+        pokeCard.appendChild(pokeBack)
+        pokeScene.appendChild(pokeCard)
+        pokeContainer.appendChild(pokeScene)
+
 }
 
 
@@ -43,4 +72,11 @@ function populatePokeCards(pokeArray) {
     <div class="card__face card__face--front">front</div>
     <div class="card__face card__face--back"><div><p>Hi,I'm here on the back</p></div></div>
   </div>
-</div>*/}
+</div>
+
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+*/}
