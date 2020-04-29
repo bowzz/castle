ecd018bd4ae346d9eac5a38ddcce8304884cebfe
Carson Hull <carsonhull@nicoles-mbp-2.ad.uvu.edu>
2020-04-29 12:20:30 -0600

    oputasyn on load page

--- pokemon/index.js
+++ pokemon/index.js
@@ -25,9 +25,9 @@ async function getAPIData(url) {
 
 function loadPage() {
     getAPIData('http://pokeapi.co/api/v2/pokemon/?&limit=25').then(
-        (data) => {
+       async  (data) => {
             for (const pokemon of data.results) {
-                getAPIData(pokemon.url).then(
+               await getAPIData(pokemon.url).then(
                     (pokeData) => {
                         populatePokeCard(pokeData)
                     }
